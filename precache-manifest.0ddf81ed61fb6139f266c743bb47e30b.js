0d554065fcc215141e9fe078507574e862f2a6a7
Shoaib <shoaib_masood@rocketmail.com>
2020-02-15 23:55:50 +0500

    Updates

---
+++ precache-manifest.0ddf81ed61fb6139f266c743bb47e30b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39e2371d61f4638d1b11269c2f926cb4",
+    "url": "/React-ToDo-App/index.html"
+  },
+  {
+    "revision": "15fcf669a3ca18ab2821",
+    "url": "/React-ToDo-App/static/js/2.fcda41dc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-ToDo-App/static/js/2.fcda41dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f93abf44143db12872e7",
+    "url": "/React-ToDo-App/static/js/main.e6deb8e2.chunk.js"
+  },
+  {
+    "revision": "bc50a3ddd7538da4b02b",
+    "url": "/React-ToDo-App/static/js/runtime-main.3c7552b0.js"
+  }
+]);
